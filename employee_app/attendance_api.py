d2fc0a263933eea45822f7e7541f39be67a2e3c4
fkalpana <support@erpgulf.com>
2023-08-27 19:15:31 +0000

    added field for driver trip api

--- employee_app/attendance_api.py
+++ employee_app/attendance_api.py
@@ -1,15 +1,18 @@
 import frappe
 
 @frappe.whitelist(allow_guest=True)
-def insert_new_trip(employee_id, trip_start_time, trip_end_time,trip_start_location = None,trip_end_location=None):
+def insert_new_trip(employee_id, trip_start_time, trip_start_km,trip_start_location = None,job_order=None, trip_type=None, vehicle_number=None, trip_status=None):
     try:
         doc = frappe.get_doc({
             "doctype": "driver trips",
             "employee_id": employee_id,
             "trip_start_time": trip_start_time,
-            "trip_end_time": trip_end_time,
+            "trip_starting_km": trip_start_km,
             "trip_start_location": trip_start_location,
-            "trip_end_location": trip_end_location,
+            "custom_job_order": job_order,
+            "custom_trip_type": trip_type,
+            "vehicle_number": vehicle_number,
+            "trip_status": trip_status
         })
         doc.insert()
         frappe.db.commit()
