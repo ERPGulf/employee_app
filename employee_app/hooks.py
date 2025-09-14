d5f3d1723fe9c39a52e0ec2446921ead66325144
fkalpana <support@ERPGulf.com>
2025-09-14 08:46:41 +0000

    Added HRMS requirement

--- employee_app/hooks.py
+++ employee_app/hooks.py
@@ -7,6 +7,7 @@ app_description = "Attendance and related submissions through mobile APP"
 app_email = "support@ERPGulf.com"
 app_license = "MIT"
 
+required_apps = ["hrms"]
 
 fixtures = [
     {"dt": "Custom Field", "filters": {"module": "Employee app for ERPNext"}},
