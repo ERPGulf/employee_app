d50a8f1cd2a72abe3ea44cf8fdcc94007821864e
Aysha-sithara <asithara@htsqatar.com>
2025-11-09 14:43:10 +0300

    Fixed QR code issues

--- employee_app/user_qa_code.py
+++ employee_app/user_qa_code.py
@@ -101,7 +101,9 @@ def create_qr_code(doc, method):
 			# frappe.throw(bytes.fromhex(''.join(tlv_array)).decode('utf-8','replace'))
 
 			import re
-			cleaned = re.sub(r"[^\x20-\x7E]", "", bytes.fromhex(''.join(tlv_array)).decode('utf-8','replace')).replace("'", "").strip()
+			# cleaned = re.sub(r"[^\x20-\x7E]", "", bytes.fromhex(''.join(tlv_array)).decode('utf-8','replace')).replace("'", "").strip()
+			cleaned = re.sub(r"[^A-Za-z0-9 :/\.=\-]+@", " ", bytes.fromhex(''.join(tlv_array)).decode('utf-8','replace')).strip()
+
 
 			
 			# frappe.throw(cleaned)
