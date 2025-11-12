7b3a170f6c55f3eec2a34af0fb535e9abd3f00bd
Aysha-sithara <hyrin@htsqatar.com>
2025-11-12 07:19:07 +0300

    updated checkin

--- employee_app/log_type.py
+++
@@ -1,8 +0,0 @@
-import frappe
-def execute():
-    frappe.db.set_value(
-        "DocField",
-        {"parent": "Employee Checkin", "fieldname": "log_type"},
-        "options",
-        "IN\nOUT\nLate Entry\nEarly Exit"
-    )
