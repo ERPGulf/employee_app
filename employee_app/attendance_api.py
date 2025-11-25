3f4f212de1e81b85fefc7c9a6caff1b263792c17
Aysha-sithara <asithara@htsqatar.com>
2025-11-25 12:31:31 +0300

    updated code

--- employee_app/attendance_api.py
+++ employee_app/attendance_api.py
@@ -343,10 +343,8 @@ def get_employee_data(employee_id=None):
         )
 
 
-@frappe.whitelist(allow_guest=True)
+@frappe.whitelist()
 def get_attendance_details(employee_id=None, limit_start=0, limit_page_length=20):
-    return "testing"
-
 
     try:
         doc = frappe.db.get_list(
